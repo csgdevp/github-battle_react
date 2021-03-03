861905234aee51b5eff6ae462a9074651580d980
Chandra Gonuguntla <csgdevp@gmail.com>
2021-03-02 19:25:29 -0500

    React_Project_GithubBattle
    React based project credit Ui#dev

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./index.css";
+import Popular from "./components/Popular";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Popular />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
